59f53aa3f292937ee5cfe584ad491fc1568613e1
ysavonenko.appwell <ysavonenko.appwell@ancestry.com>
2024-01-25 08:49:03 +0200

    Contract for Metamodel scanning with basic ORM annotations

---
+++ src/main/java/com/bibernate/hoverla/jdbc/JdbcDataType.java
@@ -0,0 +1,23 @@
+package com.bibernate.hoverla.jdbc;
+
+public enum JdbcDataType {
+
+  BOOLEAN,
+  BYTE,
+  SHORT,
+  INTEGER,
+  LONG,
+  FLOAT,
+  DOUBLE,
+  NUMERIC,
+  CHAR,
+  VARCHAR,
+  DATE,
+  TIME,
+  TIMESTAMP,
+  BINARY,
+  VARBINARY,
+  LONGVARBINARY,
+  BLOB,
+  CLOB
+}
