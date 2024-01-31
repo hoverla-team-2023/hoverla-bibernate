d6f06a2af9adcdb0e236a1dda00803c5e6894f3f
Alex <alex.soliar@1touch.io>
2024-02-01 01:04:23 +0200

    Configuration as entrypoint

--- src/main/java/com/bibernate/hoverla/configuration/config/CommonConfig.java
+++ src/main/java/com/bibernate/hoverla/configuration/config/CommonConfig.java
@@ -1,4 +1,58 @@
 package com.bibernate.hoverla.configuration.config;
 
+import com.bibernate.hoverla.exceptions.ConfigurationException;
+
+/**
+ * The CommonConfig interface defines the methods for accessing
+ * configuration details commonly required in database operations.
+ * This includes methods to get JDBC URL and generic properties.
+ */
 public interface CommonConfig {
+
+  String XML_PATTERN = "xml";
+  String YML_PATTERN = "yml";
+  String PROPERTIES_PATTERN = "properties";
+
+  /**
+   * Retrieves the value of a specified property.
+   *
+   * @param key The key of the property to retrieve.
+   *
+   * @return The value of the specified property as a String.
+   */
+  String getProperty(String key);
+
+  /**
+   * Sets the value of a specified property.
+   *
+   * @param key   The key of the property to set.
+   * @param value The value to set for the specified property.
+   */
+  void setProperty(String key, String value);
+
+  /**
+   * Loads configuration from the specified path based on the file extension.
+   * This method dynamically determines the type of configuration file
+   * (XML, YML, or properties) and returns an appropriate implementation
+   * of the CommonConfig interface that can handle that file type.
+   *
+   * @param configPath The path of the configuration file to load.
+   *
+   * @return An instance of CommonConfig suitable for the file type, or null if the file type is unsupported.
+   */
+  static CommonConfig of(String configPath) {
+    if (configPath == null) {
+      throw new IllegalStateException("File not found in the classpath");
+    }
+
+    // Determine the file extension to decide which implementation to use
+    String fileExtension = configPath.substring(configPath.lastIndexOf(".") + 1);
+
+    return switch (fileExtension) {
+      case XML_PATTERN -> new XmlConfig(configPath);
+      case YML_PATTERN -> new YmlConfig(configPath);
+      case PROPERTIES_PATTERN -> new PropertiesConfig(configPath);
+      default -> throw new ConfigurationException("Config file unsupported with extensions");
+    };
+  }
 }
--- src/main/java/com/bibernate/hoverla/configuration/config/PropertiesConfig.java
+++ src/main/java/com/bibernate/hoverla/configuration/config/PropertiesConfig.java
@@ -1,4 +1,38 @@
 package com.bibernate.hoverla.configuration.config;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import com.bibernate.hoverla.exceptions.ConfigurationException;
+
 public class PropertiesConfig implements CommonConfig {
+  private final Properties properties;
+
+  public PropertiesConfig(String name) {
+    properties = readProperties(name);
+  }
+
+  @Override
+  public String getProperty(String key) {
+    return properties.getProperty(key);
+  }
+
+  @Override
+  public void setProperty(String key, String value) {
+     properties.put(key, value);
+  }
+
+  private static Properties readProperties(String name) {
+    var properties = new Properties();
+    try (InputStream input = PropertiesConfig.class.getClassLoader().getResourceAsStream(name)) {
+      if (input != null) {
+        properties.load(input);
+        return properties;
+      }
+      throw new ConfigurationException("Properties file not found in the classpath");
+    } catch (IOException e) {
+      throw new ConfigurationException("Error loading properties file", e);
+    }
+  }
 }
