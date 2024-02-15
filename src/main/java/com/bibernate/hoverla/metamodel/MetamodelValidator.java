47d7aff2a16f6b98b8c30a5d0cefbd958f6fbefc
Bohdana Sherstyniuk <ms.sherstynyuk@gmail.com>
2024-02-15 22:43:51 +0200

    #60 add java docs

--- src/main/java/com/bibernate/hoverla/metamodel/MetamodelValidator.java
+++ src/main/java/com/bibernate/hoverla/metamodel/MetamodelValidator.java
@@ -1,7 +1,16 @@
 package com.bibernate.hoverla.metamodel;
 
+/**
+ * Interface for validating a metamodel.
+ * This interface defines a method to perform validation on a given metamodel.
+ */
 public interface MetamodelValidator {
 
+  /**
+   * Validates the provided metamodel.
+   *
+   * @param metamodel The metamodel to validate.
+   */
   void validate(Metamodel metamodel);
 
 }
